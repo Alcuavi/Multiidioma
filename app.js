de8d44acc14b93528a98567a631647310de751c4
Alberto Cuadrado <Alberto.cuvi@gmail.com>
2018-12-18 12:57:39 +0100

    configurados lenguajes a utilizar

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const I18n =require('i18n');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const I18n_routes = require('./routes/i18n');
 
 var app = express();
+I18n.configure({
+    locales:['es', 'en'],
+    cookie: 'secret-lang',
+    directory: __dirname + '/locales',
+    defaultLocale: 'es'
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
